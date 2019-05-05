6f2fa745890df729674de5cb687728f0e9fd3994
akosgarai <szlem01@gmail.com>
2019-05-06 00:30:35 +0200

    And function to cli

--- cmd/cli/app.go
+++ cmd/cli/app.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	"github.com/akosgarai/logicgates/basic"
 	"github.com/fatih/color"
 )
 
@@ -32,6 +33,7 @@ func New() *Cli {
 			{"help", "Print out the help menu."},
 			{"colors", "Print out a sample text for checking the colors that we can use."},
 			{"exit", "Quit from the application."},
+			{"and", "The and application demonstrates the And gate."},
 		},
 	}
 	c.Welcome()
@@ -51,18 +53,34 @@ func (c *Cli) PrintClolorsForTesting() error {
 	color.Yellow("LogicGate cli-tool")
 	color.Green("LogicGate cli-tool")
 	color.Red("LogicGate cli-tool")
+	c.prompt.Println("prompt style text")
+	c.welcome.Println("welcome style text")
+	c.help.Println("help style text")
+	c.invalid.Println("invalid style text")
+	fmt.Println("")
 	return nil
 }
 
 func (c *Cli) Prompt() {
-	t := time.Now()
-	c.prompt.Printf("[ %s ] cli-tool $ ", t.Format("2006-01-02 15:04:05"))
+	c.prompt.Printf("[ %s ] cli-tool $ ", time.Now().Format("2006-01-02 15:04:05"))
 }
 func (c *Cli) Help() error {
 	c.help.Println("Commands:")
 	for _, item := range c.commands {
 		c.help.Println(" - ", item.Name, " : ", item.Description)
 	}
+	fmt.Println("")
+	return nil
+}
+func (c *Cli) And(cmdString string) error {
+	commandStr := strings.TrimSuffix(cmdString, "\n")
+	arrCommandStr := strings.Fields(commandStr)
+	if len(arrCommandStr) != 3 {
+		c.invalid.Println("Wrong number of arguments. Try this: add 0 0")
+	}
+	arg1 := arrCommandStr[1] == "1"
+	arg2 := arrCommandStr[2] == "1"
+	c.welcome.Println("And(", arg1, ",", arg2, ") =", logicgates.And(arg1, arg2))
 	fmt.Println("")
 	return nil
 }
@@ -86,6 +104,8 @@ func (c Cli) RunCommand(commandStr string) error {
 		return c.PrintClolorsForTesting()
 	case "help":
 		return c.Help()
+	case "and":
+		return c.And(commandStr)
 	}
 	return c.InvalidCommand(commandStr)
 }
